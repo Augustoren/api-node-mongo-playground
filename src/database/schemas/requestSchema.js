c2925440d16f35a3aceade22a4dbc01c80a49267
Augusto Renan <augustorenanss@gmail.com>
2020-05-15 19:28:38 -0300

    Correcao middleware watcher

--- src/database/schemas/requestSchema.js
+++ src/database/schemas/requestSchema.js
@@ -4,6 +4,7 @@ const requestSchema = new connection.Schema({
   ip: String,
   method: String,
   path: String,
+  content: String,
 });
 
 const Request = connection.model("Request", requestSchema);
--- src/middlewares/watcher.js
+++ src/middlewares/watcher.js
@@ -2,10 +2,13 @@ const Request = require("../database/schemas/requestSchema");
 
 async function watcher(req, res, next) {
   const { ip, method, path } = req;
+  const content = JSON.stringify(req.body);
+  console.log(content);
   const request = new Request({
     ip,
     method,
     path,
+    content,
   });
 
   const result = await request.save();
